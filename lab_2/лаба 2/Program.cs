dfc1a63bf392d404f9bc2163e0c861b205dbdf8c
Gluzdov <78353811+Gluzdov@users.noreply.github.com>
2022-06-02 22:12:52 +0700

    Add files via upload

---
+++ lab_2/лаба 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW_2
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("x = " + new RomanNumber(21) + " y = " + new RomanNumber(17));
+            Console.WriteLine("x + y = " + RomanNumber.Add(new RomanNumber(15), new RomanNumber(10)));
+            Console.WriteLine("x - y = " + RomanNumber.Sub(new RomanNumber(15), new RomanNumber(10)));
+            Console.WriteLine("x * y = " + RomanNumber.Mul(new RomanNumber(15), new RomanNumber(10)));
+            Console.WriteLine("x / y = " + RomanNumber.Div(new RomanNumber(15), new RomanNumber(10)));
+            RomanNumber[] romanNumberArray = new RomanNumber[10];
+            for (int i = 0; i < 10; i++)
+            {
+                romanNumberArray[i] = new RomanNumber((ushort)(new Random().Next(1, 1000)));
+                Console.Write(romanNumberArray[i] + " ");
+            }
+            Console.WriteLine();
+            Array.Sort(romanNumberArray);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(romanNumberArray[i] + " ");
+            }
+        }
+    }
+}
